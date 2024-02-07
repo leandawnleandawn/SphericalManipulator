f6d78b00198f5da1a7118cd8b37867129b53aa18
leandawnleandawn <lyndonallensales@gmail.com>
2024-02-07 08:10:45 +0800

    Fixed Bugs on DH Frame and Table Generator

--- Forward Kinematics/Practice on SCARAv3/computation.py
+++ Forward Kinematics/Practice on SCARAv3/computation.py
@@ -16,20 +16,25 @@ class Robot:
 class Joint(Robot):
     '''The Following Blueprint of the Joints connecting the Robot
 	Child: Prismatic and Revolute
-    '''
-    orientation = np.array([0,0,0])
+    '''    
+    def __init__(self, z_orientation = np.array([0,0,0])):
+        self.z_orientation = z_orientation
     
 class Prismatic(Joint):
     '''The Following Blueprint of the Prismatic
 	Child: Prismatic and Revolute
     '''
-    joint_limit = [0, 5]
+    def __init__(self, joint_limit = [0,5]):
+        Joint.__init__(self, z_orientation= np.array([0,0,0]))
+        self.joint_limit = joint_limit
 
 class Revolute(Joint):
     '''The Following Blueprint of the Revolute Joints connecting the Robot
 	Child: Prismatic and Revolute
     '''
-    joint_limit = [-90, 90]
+    def __init__(self, joint_limit = [-90, 90]):
+        Joint.__init__(self, z_orientation= np.array([0,0,0]))
+        self.joint_limit = joint_limit
 
 def mainMenu():
     while True:
@@ -62,6 +67,7 @@ def createManipulator():
     robot = Robot()
     i = 0
     while True:
+        os.system("cls")
         print("We are creating a Manipulator")
         print("   What type of Joint Variable?   ")
         print("[R]          Revolute             ")
@@ -69,43 +75,44 @@ def createManipulator():
         print("[V]         View Joints           ")
         print("[-1]         End the Operation     ")
         user_input = input("...")
-        joints[i] = 0
         if user_input == 'R':
-            print("indicate what orientation of its z-axis")
+            print("indicate what z_orientation of its z-axis")
             orientation_input = input("([F]orward, [B]ackward, [L]eft, [R]ight, [U]p, [D]own)...")
             joints[i] = Revolute()
             if orientation_input == "L":
-                joints[i].orientation = np.array([1, 0, 0])
+                joints[i].z_orientation = np.array([1, 0, 0])
             if orientation_input == "R":
-                joints[i].orientation = np.array([-1, 0, 0])
+                joints[i].z_orientation = np.array([-1, 0, 0])
             if orientation_input == "F":
-                joints[i].orientation = np.array([0, -1, 0])
+                joints[i].z_orientation = np.array([0, -1, 0])
             if orientation_input == "B":
-                joints[i].orientation = np.array([0, 1, 0])
+                joints[i].z_orientation = np.array([0, 1, 0])
             if orientation_input == "U":
-                joints[i].orientation = np.array([0, 0, 1])
+                joints[i].z_orientation = np.array([0, 0, 1])
             if orientation_input == "U":
-            	joints[i].orientation = np.array([0, 0, -1])
+            	joints[i].z_orientation = np.array([0, 0, -1])
             i += 1
         if user_input == 'P':
-            print("indicate what orientation of its z-axis")
+            print("indicate what z_orientation of its z-axis")
             orientation_input = input("([F]orward, [B]ackward, [L]eft, [R]ight, [U]p, [D]own)...")
             joints[i] = Prismatic()
             if orientation_input == "L":
-                joints[i].orientation = np.array([1, 0, 0])
+                joints[i].z_orientation = np.array([1, 0, 0])
             if orientation_input == "R":
-                joints[i].orientation = np.array([-1, 0, 0])
+                joints[i].z_orientation = np.array([-1, 0, 0])
             if orientation_input == "F":
-                joints[i].orientation = np.array([0, -1, 0])
+                joints[i].z_orientation = np.array([0, -1, 0])
             if orientation_input == "B":
-                joints[i].orientation = np.array([0, 1, 0])
+                joints[i].z_orientation = np.array([0, 1, 0])
             if orientation_input == "U":
-                joints[i].orientation = np.array([0, 0, 1])
+                joints[i].z_orientation = np.array([0, 0, 1])
             if orientation_input == "U":
-            	joints[i].orientation = np.array([0, 0, -1])
+            	joints[i].z_orientation = np.array([0, 0, -1])
             i += 1
         if user_input == 'V':
-            print(joints)
+            for i,j in joints.items():
+                print(f"Joint {i+1}", end = " ")
+                print(j.z_orientation)
         if user_input == "-1":
             print("Exiting the following menu", end = " ")
             for i in range(3):

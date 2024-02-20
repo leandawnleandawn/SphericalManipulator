e1a89d209d4b153164db146a414e377b5410dfef
leandawnleandawn <lyndonallensales@gmail.com>
2024-02-20 12:15:02 +0800

    IKinGraph

--- Inverse Kinematics/IKinGraph.py
+++ Inverse Kinematics/IKinGraph.py
@@ -13,9 +13,18 @@ def defineJoints():
     z_03 = float(input("z_03 >>>"))
     return x_03, y_03, z_03
 
+def invKins(a1, a2, a3, a4, x_03, y_03, z_03):
+    phi2 = np.arctan(y_03/x_03)
+    r1 = np.sqrt((x_03^2) + (y_03^2))
+    phi1 = np.arccos((a4^2 - r1^2 - a2^2)/(-2*r1*a2))
+    theta1 = (phi2 - phi1)*(180/np.pi)
+    return theta1
+
+
 a1, a2, a3, a4 = defineJoints()
 x_03, y_03, z_03 = defineJoints()
 
-phi2 = np.arctan(y_03/x_03)
+invKins(a1, a2, a3, a4, x_03, y_03, z_03)
+
 
 
